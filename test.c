f5c982030d1d4c8c74b6c7d12154eb2be2523736
Dennis Chen <dchen043@ucr.edu>
2016-02-01 18:20:50 -0800

    Edited testcases

--- test.c
+++ test.c
@@ -1,22 +1,48 @@
 #include "types.h"
 #include "user.h"
 #include "syscall.h"
+#include "fcntl.h"
 
 int
 main(int argc, char *argv[]){
   int pid = fork();
   if(pid>0){
-    int i;
-    for(i=0;i<200;i++)
-      printf(1,"Parent %d, Tickets %d\n",i,1);
+    int pid2 = fork();
+    if(pid2>0){
+      status(pid);
+      status(pid2);
+      sleep(500);
+      kill(pid);
+      kill(pid2);
+      status(pid);
+      status(pid2);
+      wait();
+    }
+    else{
+      int tickets = 1;
+      settickets(tickets);
+      int fd = open("testa",O_CREATE|O_RDWR);
+      write(fd,"hello\n",6);
+      close (fd);
+      while(1){
+        int fd2 = open("testa",O_RDWR);
+        char str[6];
+        read(fd2,str,6);
+        write(fd2,str,6);
+        close(fd2);
+      }
+    }
     wait();
   }
   else{
-    int tickets = 20000;
+    int tickets = 1;
     settickets(tickets);
-    int i;
-    for(i=0;i<200;i++)
-      printf(1,"Child %d, Tickets %d\n",i,tickets);
+    int i=1;
+    int j= 1;
+    while(1){
+      i=i+j;
+      j=i+j;
+    }
   }
   exit();
 }
--- test2.c
+++ test2.c
@@ -2,12 +2,14 @@
 #include "user.h"
 #include "syscall.h"
 
+
 int
 main(int argc, char *argv[]){
   int pid = fork();
   if(pid>0){
     int pid2 = fork();
     if(pid2>0){
+      sleep(50);
       status(pid);
       status(pid2);
       sleep(500);
@@ -15,11 +17,10 @@ main(int argc, char *argv[]){
       kill(pid2);
       status(pid);
       status(pid2);
-      printf(1,"%d %d\n",pid ,pid2);
       wait();
     }
     else{
-      int tickets = 1;
+      int tickets = 10;
       settickets(tickets);
       while(1){
       }
@@ -27,10 +28,11 @@ main(int argc, char *argv[]){
     wait();
   }
   else{
-    int tickets = 20;
+    int tickets =50;
     settickets(tickets);
     while(1){
     }
   }
   exit();
 }
+
